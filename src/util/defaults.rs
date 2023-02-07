8f0dd27b55be42a6ca528ab6ac8ff1d509777f3a
Luis Schweigard <luis.schweigard@gmail.com>
2023-02-07 16:18:07 +0100

    Fix handling of core apiGroup in audit and evaluation (#4)
    Co-authored-by: Sebastian Woehrl <sebastian.woehrl@maibornwolff.de>

--- src/util/defaults.rs
+++
@@ -1,8 +0,0 @@
-/// Returns the original api_group if it was not empty, otherwise returns "core"
-pub fn api_group_or_default(api_group: &str) -> &str {
-    if api_group.is_empty() { 
-        "core" 
-    } else { 
-        api_group 
-    }
-}
